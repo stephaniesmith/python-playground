e507bd5187cf32b0977c95b290a90084b912cb94
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-06 08:11:51 -0800

    getting the deck class working

--- black_jack/blackjack.py
+++ black_jack/blackjack.py
@@ -22,9 +22,29 @@ playing = True
 
 class Card:
   def __init__(self, suit, rank):
-    self.suit = suits
+    self.suit = suit
     self.rank = rank
 
   def __str__(self):
     return self.rank + ' of ' + self.suit
 
+class Deck:
+
+  def __init__(self):
+    self.deck = []
+    for suit in suits:
+      for rank in ranks:
+        self.deck.append(Card(suit, rank))
+
+  def __str__(self):
+    deck_comp = [card.__str__() for card in self.deck]
+    return 'The deck has: ' + '\n'.join(deck_comp)
+
+  def shuffle(self):
+    random.shuffle(self.deck)
+
+  def deal(self):
+    return self.deck.pop()
+
+test_deck = Deck()
+print(test_deck)
