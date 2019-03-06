706c51453a204b195ef79cdb7b656a2a830ee2a3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-06 08:29:05 -0800

    hand and chip classes

--- black_jack/blackjack.py
+++ black_jack/blackjack.py
@@ -46,5 +46,52 @@ class Deck:
   def deal(self):
     return self.deck.pop()
 
+
+class Hand:
+
+  def __init__(self):
+    self.cards = []
+    self.value = 0
+    self.aces = 0
+
+
+  def add_card(self, card):
+    self.cards.append(card)
+    self.value += values[card.rank]   
+
+    if card.rank == 'Ace':
+      self.aces += 1
+
+  def adjust_for_ace(self):
+    while self.value > 21 and self.aces:
+      self.value -= 10
+      self.aces -= 1
+
+  class Chips:
+
+    def __init__(self, total = 100):
+      self.total = total
+      self.bet = 0
+
+    def win_bet(self):
+      self.total += self.bet
+
+    def lose_bet(self):
+      self.total -= self.bet
+
+
 test_deck = Deck()
-print(test_deck)
+test_deck.shuffle()
+
+test_player = Hand()
+pulled_card = test_deck.deal()
+print(pulled_card)
+
+test_player.add_card(pulled_card)
+print(test_player.value)
+
+pulled_card = test_deck.deal()
+print(pulled_card)
+
+test_player.add_card(pulled_card)
+print(test_player.value)
