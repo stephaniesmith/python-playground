4b56dd9662d98d81ae95ff8197d153eeab17310d
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-05 19:11:14 -0800

    leaning about importing

---
+++ one.py
@@ -0,0 +1,11 @@
+#one.py
+
+def func():
+    print('FUNC() IN ONE.PY')
+
+print('TOP LEVEL ONE.PY')
+
+if __name__ == '__main__':
+    print('ONE.PY RUNNING!')
+else:
+    print('ONE.PY IMPORTED!')
---
+++ two.py
@@ -0,0 +1,11 @@
+import one
+
+print('TOP LEVEL TWO.PY')
+
+one.func()
+
+
+if __name__ == '__main__':
+    print('TWO.PY RUNNING!')
+else:
+    print('TWO.PY IMPORTED!')
