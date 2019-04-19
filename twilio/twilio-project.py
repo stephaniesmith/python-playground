61fadf9908d80a33589a1b8f6136a2af9297bd3a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-19 07:41:24 -0700

    trying to figure out twilo

---
+++ twilio/twilio-project.py
@@ -0,0 +1,12 @@
+# from twilio.rest import TwilioRestClient
+from .env import SID, AUTH_TODKEN
+
+print(SID)
+
+
+# account_sid = SID
+# auth_token = AUTH_TODKEN
+# client = TwilioRestClient(account_sid, auth_token)
+
+# call = client.calls.get("CA42ed11f93dc08b952027ffbc406d0868")
+# print(call.to)
